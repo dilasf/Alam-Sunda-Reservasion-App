e42b5d4313df03f1a5479807a96c83e8d22a3be9
sifa fadilah <sifafadilah339@gmail.com>
2024-12-16 21:35:36 +0700

    vercel commit

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Initialize autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Handle request
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+$request = Illuminate\Http\Request::capture();
+
+// Pass request to the application
+$app->run($request);
